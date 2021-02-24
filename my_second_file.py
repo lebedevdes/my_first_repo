76ccd598ec23e73f3f0153ec3dbd81350f66ff2e
Anton Lebedev <lebedevdes@gmail.com>
2021-02-24 16:35:50 +0300

    Refactoring

--- my_second_file.py
+++ my_second_file.py
@@ -1,4 +1,15 @@
-def square(x):
-    # Возводит в квадрат x
+"""Модуль с функцией возвредения в квадрат"""
+from typing import Number
+
+
+def square(x: Number) -> Number:
+    """
+    Возводит в квадрат x
+
+    Args:
+        x: входное число
+    Returns:
+        результат возведения в квадрат
+    """
     return x*x
 
